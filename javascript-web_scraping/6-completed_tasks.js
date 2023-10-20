710885014e226c48347e5229fff133989a35ece5
juanRCoder <guillecarck@gmail.com>
2023-10-20 23:44:02 +0000

    feat: count user that completed task

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, res, body) => {
+  if (error) console.error(error);
+  
+  const data = JSON.parse(body);
+  const results = {};
+
+  data.forEach(task => {
+    if (task.completed) {
+      if (results[task.userId]) results[task.userId]++;
+      else results[task.userId] = 1;
+    }
+  });
+
+  console.log(results);
+
+});
