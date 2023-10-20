2f602da6c531f5d3f10e25e52918d858fd611004
juanRCoder <guillecarck@gmail.com>
2023-10-20 23:26:37 +0000

    feat: get the contents of a webpage

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const path = process.argv[3];
+
+request(url, (error, res, body) => {
+  if (error) console.error(error);
+  else {
+    fs.writeFile(path, body, 'utf-8', (e) => {
+      if (e) console.error(e);
+    });
+  }
+});
