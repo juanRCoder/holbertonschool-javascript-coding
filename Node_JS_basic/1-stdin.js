a93e29babd0926b4ac83b330fc8276617539cb63
juanRCoder <guillecarck@gmail.com>
2023-11-01 01:44:31 +0000

    fix: fixing input in process.stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data',(data) => {
-  process.stdout.write(`Your name is: ${data}`);
+process.stdin.on('data', (input) => {
+  process.stdout.write(`Your name is: ${input}`);
   process.stdin.end();
 });
 process.on('exit', () => {
