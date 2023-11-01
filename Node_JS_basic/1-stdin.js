e2e96324471b12c725f9b2a983959dde2e2c2629
juanRCoder <guillecarck@gmail.com>
2023-11-01 01:09:24 +0000

    fix: fixing input for cheked is not empty

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,8 +5,16 @@ const message = readline.createInterface({
   output: process.stdout,
 });
 
-message.question('Welcome to Holberton School, what is your name?\n', (res) => {
-  console.log(`Your name is: ${res}`);
+console.log('Welcome to Holberton School, what is your name?');
+
+message.on('line', (res) => {
+ if (res.trim() === ''){
+   message.close();
+   return;
+ }
+
+  const name = res.trim();
+  console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
   message.close();
 });
