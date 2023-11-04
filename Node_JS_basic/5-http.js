ed5ca12813d35199a38e57d6482dabb570b5e86b
Juan Ramirez <124215732+juanRCoder@users.noreply.github.com>
2023-11-04 10:46:21 -0500

    feat: add new API for display content of the database file

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,38 +2,33 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const port = 1245;
+const database = process.argv[2] || 'database.csv';
 
-const handleRequest = async (req, res) => {
+const app = http.createServer((req, res) => {
   const { url } = req;
-  let response = '';
 
-  try {
-    if (url === '/') {
-      response = 'Hello Holberton School!';
-    } else if (url === '/students') {
-      const data = await countStudents('database.csv');
-      const { students } = data;
-      response = `This is the list of our students\n
-        Number of students: ${data.totalStudents}\n
-        Number of students in CS: ${students.CS.count}. List: ${students.CS.list.join(', ')}\n       
-        Number of students in SWE: ${students.SWE.count}. List: ${students.SWE.list.join(', ')}`;    
-    } else {
-      res.statusCode = 404;
-      response = 'Not Found';
-    }
-  } catch (error) {
-    console.error(error);
-    res.statusCode = 500;
-    response = 'Internal Server Error';
-  } finally {
-    res.end(response);
+  if (url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    countStudents(database)
+      .then((data) => {
+        const { students } = data;
+        const response = `This is the list of our students\n
+          Number of students: ${data.totalStudents}\n
+          Number of students in CS: ${students.CS.count}. List: ${students.CS.list.join(', ')}\n
+          Number of students in SWE: ${students.SWE.count}. List: ${students.SWE.list.join(', ')}`;
+        res.end(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } else {
+    res.end('Not Found');
   }
-};
-
-const app = http.createServer(handleRequest);
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${PORT}`);                                                                           23,4          All 
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
