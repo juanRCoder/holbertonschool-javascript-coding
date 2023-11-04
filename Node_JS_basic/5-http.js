4c140da0c6f30c7e1477db801c9dec7c6ccf7ca4
Juan Ramirez <124215732+juanRCoder@users.noreply.github.com>
2023-11-04 10:31:37 -0500

    feat: create server with API's and function async

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,29 +1,39 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const port = 1245;
+
+const handleRequest = async (req, res) => {
   const { url } = req;
+  let response = '';
 
-  if (url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (url === '/students') {
-    countStudents('database.csv')
-      .then((data) => {
-        const { students } = data;
-        const response = `This is the list of our students\n
-          Number of students: ${data.totalStudents}\n
-          Number of students in CS: ${students.CS.count}. List: ${students.CS.list.join(', ')}\n        
-          Number of students in SWE: ${students.SWE.count}. List: ${students.SWE.list.join(', ')}`;     
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(response);
-      }).catch((e) => {
-        console.error(e);
-      });
+  try {
+    if (url === '/') {
+      response = 'Hello Holberton School!';
+    } else if (url === '/students') {
+      const data = await countStudents('database.csv');
+      const { students } = data;
+      response = `This is the list of our students\n
+        Number of students: ${data.totalStudents}\n
+        Number of students in CS: ${students.CS.count}. List: ${students.CS.list.join(', ')}\n       
+        Number of students in SWE: ${students.SWE.count}. List: ${students.SWE.list.join(', ')}`;    
+    } else {
+      res.statusCode = 404;
+      response = 'Not Found';
+    }
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    response = 'Internal Server Error';
+  } finally {
+    res.end(response);
   }
-});
+};
 
-app.listen(1245, () => {
-  console.log('Server running on port 1245');
+const app = http.createServer(handleRequest);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${PORT}`);                                                                           23,4          All 
 });
 
 module.exports = app;
