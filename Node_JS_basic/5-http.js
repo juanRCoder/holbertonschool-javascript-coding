c394cd68ba1d67f592625e4fe37065f50ef9dff2
Juan Ramirez <124215732+juanRCoder@users.noreply.github.com>
2023-11-04 11:43:59 -0500

    Update 5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const port = 1245;
-const database = process.argv[2] || 'database.csv';
+const PORT = 1245;
+const databaseFile = process.argv[2] || 'database.csv';
 
 const app = http.createServer((req, res) => {
   const { url } = req;
@@ -11,25 +11,30 @@ const app = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (url === '/students') {
-    countStudents(database)
+    countStudents(databaseFile)
       .then((data) => {
         const { students } = data;
-        const response = `This is the list of our students\n
-          Number of students: ${data.totalStudents}\n
-          Number of students in CS: ${students.CS.count}. List: ${students.CS.list.join(', ')}\n
-          Number of students in SWE: ${students.SWE.count}. List: ${students.SWE.list.join(', ')}`;
+        const response = `This is the list of our students\nNumber of students: ${
+          data.totalStudents
+        }\nNumber of students in CS: ${students.CS.count}. List: ${students.CS.list.join(', ')}\nNumber of students in SWE: ${
+          students.SWE.count
+        }. List: ${students.SWE.list.join(', ')}`;
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end(response);
       })
       .catch((error) => {
         console.error(error);
         res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
       });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
